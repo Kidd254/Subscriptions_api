5605f1a7c2e69db46c44837a62275b7d2967b60c
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-13 20:46:31 +0300

    Add additional collumns to user table

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module SignupLogin
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exist?(env_file)
     end
     
     # config.time_zone = "Central Time (US & Canada)"
