75dcad1e5f45ecde3df664bbea40b493d35f07d8
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-13 12:28:46 +0300

    Add login parameter

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,16 +8,17 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
+      status: { code: 200, message: 'Logged in successfully.' },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
   end
 
+  # Handle the response after destroying a session (logout)
   def respond_to_on_destroy
     if current_user
       render json: {
         status: 200,
-        message: 'logged out successfully'
+        message: 'Logged out successfully'
       }, status: :ok
     else
       render json: {
@@ -25,5 +26,12 @@ class Users::SessionsController < Devise::SessionsController
         message: "Couldn't find an active session."
       }, status: :unauthorized
     end
+  end
+
+  private
+
+  # Strong parameters for user login
+  def sign_in_params
+    params.require(:user).permit(:email, :password)
   end
 end
