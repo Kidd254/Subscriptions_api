3084d67643de2d7a18c9671b74a4c1bbaccf43d0
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-13 20:36:38 +0300

    Create mpesa controller and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :mpesas
   get '/current_user', to: 'current_user#index'
 
   devise_for :users, path: '', path_names: {
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  post 'stkpush', to: 'mpesas#stkpush'
+  post 'stkquery', to: 'mpesas#stkquery'
 
   resources :users do
     resources :packages, only: [:create]
